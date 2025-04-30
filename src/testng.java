9a7f6970b7c03d62bd6ec6c5762e887d519806db
Janani-saravanan1417 <jananisaravanan1417@gmail.com>
2025-05-01 01:04:49 +0530

    Update testng.java

--- src/testng.java
+++ src/testng.java
@@ -1,1 +1,43 @@
+package utils;
 
+import org.testng.*;
+import org.testng.ITestListener;
+import com.aventstack.extentreports.*;
+import org.openqa.selenium.WebDriver;
+
+public class TestListener implements ITestListener {
+
+    private static ExtentReports extent = ExtentReportManager.getInstance();
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest extentTest = extent.createTest(result.getMethod().getMethodName());
+        test.set(extentTest);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.get().pass(" Test Passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        Object testClass = result.getInstance();
+        WebDriver driver = ((tests.BaseTest) testClass).driver;
+
+        String screenshotPath = ScreenshotUtil.capture(driver, result.getMethod().getMethodName());
+        test.get().fail(result.getThrowable());
+        test.get().addScreenCaptureFromPath(screenshotPath);
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        test.get().skip(result.getThrowable());
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+    }
+}
